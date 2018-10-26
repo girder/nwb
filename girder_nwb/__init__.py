c4feb0f112647804aefe8935989fa50afdc33bb2
Doruk Ozturk <doruk.ozturk@kitware.com>
2018-10-26 07:57:48 -0400

    Bind nwb specific handler to the finalizeUpload event

--- girder_nwb/__init__.py
+++ girder_nwb/__init__.py
@@ -1,10 +1,25 @@
 from girder import plugin
+from girder import events
+
+import pluggy
+
+from .plugin_registry import get_plugin_manager
+
+hookimpl = pluggy.HookimplMarker("nwb_girder")
+
+
+def nwb_handler(event):
+    extensions = [i.lower() for i in event.info["file"]["exts"]]
+    if "nwb" in extensions:
+        plugin_manager = get_plugin_manager()
+        plugin_manager.hook.get_metadata(path="foobar")
+        plugin_manager.hook.get_thumbnail(path="foobar")
+    return event
 
 
 class GirderPlugin(plugin.GirderPlugin):
-    DISPLAY_NAME = 'nwb',
-    CLIENT_SOURCE_PATH = 'web_client'
+    DISPLAY_NAME = ("nwb",)
+    CLIENT_SOURCE_PATH = "web_client"
 
     def load(self, info):
-        # add plugin loading logic here
-        pass
+        events.bind("model.file.finalizeUpload.after", "nwb_handler", nwb_handler)
